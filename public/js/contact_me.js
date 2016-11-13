647039c965de21b1207c7176dd7aed6084312267
Wesley Smith <wesleyfsmith@gmail.com>
2016-11-13 12:30:24 -0700

    Initial commit and setup

---
+++ public/js/contact_me.js
@@ -0,0 +1,75 @@
+// Contact Form Scripts
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://ubi-mailer.herokuapp.com/ubi_mailer/send_default",
+                type: "POST",
+                dataType: "jsonp",
+                data: {
+                  ubi_mail: {
+                    name: name,
+                    phone: phone,
+                    email_address: email,
+                    message: message,
+                    config_token: 'c38ac97e93e89dff61d54e4c889b87b2:cc93106104cee2d8fc24c814c15f4800f1ef4d09200310ad8fc58418fc802a221eed7164c74e156b956ec50ef9fbf97008735f5c13036f66c6d11a50feaf55de3612ff8f16eee85317719ca79f9d5693'
+                  }
+                },
+                cache: false,
+                success: function(data) {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function(data) {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
---
+++ public/js/overide.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+  // rotater on main page
+  // var talents = [
+  //   'We work with great companies.',
+  //   'We design beautiful products.',
+  //   'We build popular websites.',
+  //   'We won\'t do your yardwork.',
+  //   'We manage diversified teams.',
+  //   'But most of all, we\'re here to help'
+  // ]
+  // var count = 0;
+  // $('#rotate-sentance').html(talents[count])
+  // var rotate = function(talents){
+  //   setTimeout(
+  //     function(){
+  //       count > talents.length - 1 ? count = 0 : count++;
+  //        $('#rotate-sentance').html(talents[count])
+  //        rotate(talents);
+  //     }, 3000);
+  // }
+  // rotate(talents);
+  // end rotater
+
+
+
+
+});
